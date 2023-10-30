4efda6e35badcfdafbaca8a0be54a05949fc1845
ecargnello <emiliano.cargnello@i2t-sa.com>
2023-10-29 22:40:41 -0300

    Creacion del componente login

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,36 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserCredential } from 'firebase/auth';
+
+// * Services
 import { UserService } from 'src/app/core/services/user.service';
+import { UtilService } from 'src/app/core/services/util.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  formGroup: FormGroup;
-
-  constructor(private userService: UserService){
+  public formGroup: FormGroup;
+  public isLogged: boolean = false;
+  constructor(private _userService: UserService,
+              private _utilService: UtilService){
     this.formGroup = new FormGroup({
       email: new FormControl(),
       password: new FormControl()
     })
   }
-  
-  ngOnInit(): void {
-      
-  }
 
   onSubmit(){
-    this.userService.register(this.formGroup)
-    .then(res => {
-      console.log(res);
-      
+   this.login(this._userService.login(this.formGroup.value))
+  }
+
+  loginWithGoogle(){
+    this.login(this._userService.loginWithGoogle())
+  }
+
+  private login(promise: Promise<UserCredential>){
+    promise.then(res => {
+      if(res){
+        this._utilService.notification('Logeado correctamente!', 'success', 5000)
+        this._userService.setAuthenticated(true)
+      }
     })
     .catch(err => {
-      console.log(err);
-      
+      this._userService.setAuthenticated(false)
+      this._utilService.notification(err, 'error', 5000)
     })
   }
 }
